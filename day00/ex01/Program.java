9e72d5d8a0cf0a803f6596ca58503d6369b83333
wleite <wleite@student.42sp.org.br>
2023-04-18 12:51:32 -0300

    chore: change tabs for spaces identation

--- day00/ex01/Program.java
+++ day00/ex01/Program.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 public class Program {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int number = Integer.parseInt(scanner.nextLine());
-		if (number < 2) {
-			System.err.println("IllegalArgument");
-			System.exit(-1);
-		}
-		int div = 0;
-		int i = 1;
-		for (; i <= number; i++) {
-			if (number % i == 0)
-				div++;
-			if (div > 2) {
-				System.out.println("false " + (i - 1));
-				System.exit(0);
-			}
-		}
-		System.out.println("true " + (i - 1));
-		System.exit(0);
-	}
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int number = Integer.parseInt(scanner.nextLine());
+    if (number < 2) {
+      System.err.println("IllegalArgument");
+      System.exit(-1);
+    }
+    int div = 0;
+    int i = 1;
+    for (; i <= number; i++) {
+      if (number % i == 0)
+        div++;
+      if (div > 2) {
+        System.out.println("false " + (i - 1));
+        System.exit(0);
+      }
+    }
+    System.out.println("true " + (i - 1));
+    System.exit(0);
+  }
 }
